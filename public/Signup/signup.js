de78a235ff54757fe4b08f7d8334839cd0b4b623
vgnsh1999 <vigneshvarathan@gmail.com>
2023-07-29 08:40:18 +0530

    Sign Up Frontend

---
+++ public/Signup/signup.js
@@ -0,0 +1,22 @@
+async function signUp(event){
+    try{
+        event.preventDefault();
+        const obj = {
+            username:event.target.username.value,
+            email:event.target.email.value,
+            phonenumber:event.target.phonenumber.value,
+            password:event.target.password.value
+        }
+        const response = await axios.post('http://localhost:3000/user/signup',obj);
+        if(response.status === 201){
+          window.location.href = "../Login/login.html";
+          localStorage.setItem('username',obj.username);
+        } else {
+          throw new Error('Failed to login');
+        }
+    } catch(error){
+        console.log(error);
+        //document.body.innerHTML = document.body.innerHTML + '<h4>Something went wrong!</h4>';
+        document.body.innerHTML = document.body.innerHTML + `<div style="color:red";>${error}</div>`
+    }
+}
